510c9fc78eb5d472eeacd37bb9a7144c0519b777
Sybren W <sybren.willemot@gmail.com>
2025-02-09 00:24:17 +0100

    refactor(packages): update avatar examples and stories

--- packages/vue/src/components/avatar/examples/basic.vue
+++ packages/vue/src/components/avatar/examples/basic.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { Avatar } from '@ark-ui/vue/avatar'
+import { Avatar } from "@ark-ui/vue/avatar";
 </script>
 
 <template>
   <Avatar.Root>
     <Avatar.Fallback>PA</Avatar.Fallback>
-    <Avatar.Image src="https://i.pravatar.cc/3000" alt="avatar" />
+    <Avatar.Image src="https://avatars.githubusercontent.com/u/1846056?v=4" alt="avatar" />
   </Avatar.Root>
 </template>
