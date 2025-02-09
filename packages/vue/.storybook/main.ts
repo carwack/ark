0b80db658e15d8307352a460e614a152a2cbe6f0
Sybren W <sybren.willemot@gmail.com>
2025-02-09 21:50:37 +0100

    feat(vue): add storybook for stories

--- packages/vue/.storybook/main.ts
+++ packages/vue/.storybook/main.ts
@@ -1,1 +1,38 @@
-import './main.css'
+import type { StorybookConfig } from "@storybook/vue3-vite";
+
+const config: StorybookConfig = {
+	stories: ["../src/**/*.stories.ts"],
+	addons: [
+		{
+			name: "@storybook/addon-essentials",
+			options: { backgrounds: false, controls: false, actions: false },
+		},
+		"@storybook/addon-a11y",
+	],
+	framework: {
+		name: "@storybook/vue3-vite",
+		options: {},
+	},
+	core: {
+		disableTelemetry: true,
+	},
+	docs: {
+		autodocs: false,
+	},
+	viteFinal(config) {
+		const vueDocgenIndex = config.plugins?.findIndex(
+			(plugin) =>
+				(plugin as { name: string }).name === "storybook:vue-docgen-plugin",
+		);
+		if (
+			vueDocgenIndex !== undefined &&
+			vueDocgenIndex !== -1 &&
+			config.plugins
+		) {
+			config.plugins.splice(vueDocgenIndex, 1);
+		}
+		return config;
+	},
+};
+
+export default config;
